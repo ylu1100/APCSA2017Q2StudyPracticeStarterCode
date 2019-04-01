37073bc84c204a4f69b9e52ceaf8f6af62cfbaa7
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-04-01 08:38:17 -0400

    Update MultPractice.java

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,17 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice {
+    private int int1;
+    private int int2;
+    public MultPractice(int int1, int int2){
+        this.int1=int1;
+        this.int2=int2;
+    }
+    public String getProblem(){
+        return (this.int1+" times "+this.int2);
+    }
+    public void nextProblem(){
+        this.int2++;
+    }
 }
